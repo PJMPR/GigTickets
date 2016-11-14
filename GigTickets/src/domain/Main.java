2c80c1797bad56def146974e652b83f8972c2e17
zielonyhols <zielonyhools@gmail.com>
2016-11-13 20:45:22 -0800

    first build

--- GigTickets/src/domain/Main.java
+++ GigTickets/src/domain/Main.java
@@ -1,10 +1,38 @@
 package domain;
 
+//  system rezerwacji biletow na koncerty
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import domain.Ticket.Category;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/workdb");
+			TicketRepository ticketRepository = new TicketRepository(connection);
+			Ticket ticket1 = new Ticket();
+			ticket1.setCategory(Category.rap);
+			ticket1.setName("tede");
+			ticket1.setDate("11-11-2016");
+			ticket1.setLocation("Warszawa");
+			ticket1.setPrice(30);
+			ticket1.setQuantity(500);
+			
+			// sticketRepository.add(ticket1);
+			
+			Ticket ticket1FromDb = ticketRepository.get(0);
+			System.out.println("Kategoria: " + ticket1FromDb.getCategory() 
+			+ ", nazwa: " + ticket1FromDb.getName() 
+			+ ", data: " + ticket1FromDb.getDate()
+			+ ", lokalizacja: " + ticket1FromDb.getLocation()
+			+ ", cena: " + ticket1FromDb.getPrice()
+			+ ", ilosc sztuk: " + ticket1FromDb.getQuantity());
+			System.out.print("End");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
---
+++ GigTickets/src/domain/Ticket.java
@@ -0,0 +1,72 @@
+package domain;
+
+public class Ticket {
+
+	private int id;
+	private Category category;
+	private String name;
+	private String date;
+	private String location;
+	private int price;
+	private int quantity; // ilosc
+
+	public enum Category {
+		rock, hard, heavy, pop, jazz, blues, elektro, techno, rap, reggae, pozostale;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
